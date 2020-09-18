1765196ea1efd257256cac511fdca4b11a8e8d8f
Ananth <ajonnavittula@wpi.edu>
2020-09-18 14:20:57 -0400

    Dev: Lots of training and plots

--- scripts/demo.py
+++ scripts/demo.py
@@ -1,28 +1,47 @@
-# Script to run env with a particular q network
 import gym
-import LunarLander_c1
 import torch
-from train import QNetwork
 import numpy as np
+from train import QNetwork
+import matplotlib.pyplot as plt
+import LunarLander_c1
+import pickle
+import random
 
-if __name__ == "__main__":
+def get_model_name(r_type):
+    # R1 with no time stop 
+    model_name = "dqn_R1.pth"
+    # R2 with no time stops (counter factuals for R2)
+    if r_type == 2:
+        model_name = "dqn_R2.pth"
+    # R3 with no time stops (counter factuals for R3)
+    elif r_type == 3:
+        model_name = 'dqn_R3.pth'
+
+    return model_name
+
+def generate_data(r_type, ep):
+    
+    model_name = get_model_name(r_type)
     env = gym.make('LunarLanderC1-v0')
-    env.seed(0)
-    reward_type = 1
-    episodes = 25
-    model_name = 'dqn_R' + str(reward_type) + '.pth'
-    # model_name = 'dqn.pth'
+
+    # load our trained q-network
     path = "../models/" + model_name
     qnetwork = QNetwork(state_size=8, action_size=4, seed=1)
     qnetwork.load_state_dict(torch.load(path))
     qnetwork.eval()
     softmax = torch.nn.Softmax(dim=1)
 
-    for episode in range(episodes):
-        state = env.reset(reward_type=reward_type)
-        score = 0
+    # we'll rollout over N episodes
+    episodes = ep
+    score = 0
+
+    for episode in range(episodes):   
+        # reset to start
+        state = env.reset(reward_type=r_type)
+        episode_reward = 0
+
         for t in range(1000):
-            # get the best action according to q-network
+        # get the best action according to q-network
             with torch.no_grad():
                 state_t = torch.from_numpy(state).float().unsqueeze(0)
                 action_values = qnetwork(state_t)
@@ -31,14 +50,22 @@ if __name__ == "__main__":
 
             # apply that action and take a step
             env.render()              # can always toggle visualization
-            next_state, reward, done, info = env.step(action)
+            next_state, _, done, info = env.step(action)
+            reward = info['reward']
+            awake = info['awake']
+            state = next_state
             score += reward
-            print(reward)
-            print(info['reward'])
-            print(info['mod_reward'])
-            print(info['reward_type'])
+            episode_reward += reward
+
             if done:
                 break
-        # print(score)
-
     env.close()
+
+def main():
+    eps = 10
+    i = 3
+    generate_data(i, eps)    
+
+if __name__ == "__main__":
+    main()
+
--- scripts/learn.py
+++ scripts/learn.py
@@ -8,28 +8,34 @@ import matplotlib.pyplot as plt
 import pickle
 import random
 
-
-
 # Reward functions
-
-def Reward(xi, R_type):
+def Reward(xi, r_type):
     R = 0
     shaping = 0
     prev_shaping = None
     reward = 0
+    initial_waypoint = xi[0]
+    initial_state = initial_waypoint[3]
+    initial_x = initial_state[0]
     for i, waypoint in enumerate(xi):
         state = waypoint[3]
         action = waypoint[1]
-        if R_type == 1:
+        if r_type == 1:
             shaping = \
-                - 200*np.sqrt(state[0]*state[0] + state[1]*state[1]) \
+                - 100*np.sqrt(state[0]*state[0] + state[1]*state[1]) \
                 - 100*np.sqrt(state[2]*state[2] + state[3]*state[3]) \
                 - 100*abs(state[4]) + 10*state[6] + 10*state[7]
+        elif r_type == 2:
+            shaping = \
+                - 100*np.sqrt(state[1]*state[1]) \
+                - 100*np.sqrt(state[2]*state[2] + state[3]*state[3]) \
+                - 100*abs(state[4]) + 10*state[6] + 10*state[7]\
+                - 10*abs(abs(state[0]) - abs(initial_x)) 
         else:
             shaping = \
-                - 200*np.sqrt(state[1]*state[1]) \
+                - 100*np.sqrt(state[0]*state[0] + state[1]*state[1]) \
                 - 100*np.sqrt(state[2]*state[2] + state[3]*state[3]) \
-                - 100*abs(state[4]) + 10*state[6] + 10*state[7]
+                - 0*abs(state[4]) + 0*state[6] + 0*state[7]  
 
         if prev_shaping is not None:
             reward = shaping - prev_shaping
@@ -40,38 +46,41 @@ def Reward(xi, R_type):
             
         elif action == 2:
             reward -= 0.30
-            
-        # if not action == 0:
-        #     reward += 1
 
         awake = waypoint[2]
         if i == len(waypoint) and awake:
-            reward += 100
+            if r_type == 3:
+                reward = -100
+            else:
+                reward = +100
         elif not awake:
-            reward -= 100
+            if r_type == 3:
+                reward = +100
+            else:
+                reward = -100
         R += reward
     return R
 
 def get_belief(beta, D, Xi_R):
-
     # Reward for landing in the center
     rewards_D = np.asarray([Reward(xi,1) for xi in D], dtype = np.float32)    
     rewards_XiR = np.asarray([Reward(xi,1) for xi in Xi_R], dtype = np.float32)    
     rewards_D_2 = np.asarray([Reward(xi,2) for xi in D], dtype = np.float32)    
     rewards_XiR_2 = np.asarray([Reward(xi,2) for xi in Xi_R], dtype = np.float32)
+    rewards_D_3 = np.asarray([Reward(xi,3) for xi in D], dtype = np.float32)    
+    rewards_XiR_3 = np.asarray([Reward(xi,3) for xi in Xi_R], dtype = np.float32)
 
     norm = np.max(np.asarray([np.max(np.abs(rewards_D)),np.max(np.abs(rewards_XiR)),\
-         np.max(np.abs(rewards_D_2)), np.max(np.abs(rewards_XiR_2))]))
-    
-    rewards_D = rewards_D/np.max(np.abs(rewards_D))
-    rewards_XiR = rewards_XiR/np.max(np.abs(rewards_XiR))
-    rewards_D_2 = rewards_D_2/np.max(np.abs(rewards_D_2))
-    rewards_XiR_2 = rewards_XiR_2/np.max(np.abs(rewards_XiR_2))
+         np.max(np.abs(rewards_D_2)), np.max(np.abs(rewards_XiR_2)),\
+         np.max(np.abs(rewards_D_3)), np.max(np.abs(rewards_XiR_3))]))
 
-    # print("Rewards_D: ", rewards_D)
-    # print("Rewards_XiR: ", rewards_XiR)
-    # print("rewards_D_2: ", rewards_D_2)
-    # print("Rewards_XiR_2: ", rewards_XiR_2)
+    rewards_D = rewards_D/norm #np.max(np.abs(rewards_D))
+    rewards_XiR = rewards_XiR/norm #np.max(np.abs(rewards_XiR))
+    rewards_D_2 = rewards_D_2/norm #np.max(np.abs(rewards_D_2))
+    rewards_XiR_2 = rewards_XiR_2/norm #np.max(np.abs(rewards_XiR_2))
+    rewards_D_3 = rewards_D_3/norm #np.max(np.abs(rewards_D_3))
+    rewards_XiR_3 = rewards_XiR_3/norm #np.max(np.abs(rewards_XiR_3))
+
 
     n1 = np.exp(beta*sum(rewards_D))
     d1 = sum(np.exp(beta*rewards_XiR))**len(D)
@@ -82,23 +91,33 @@ def get_belief(beta, D, Xi_R):
     d2 = sum(np.exp(beta*rewards_XiR_2))**len(D)
     p2 = n2/d2
 
-    Z = p1 + p2
-    b = [p1/Z, p2/Z]
+    # Reward for crashing in middle
+    n3 = np.exp(beta*sum(rewards_D_3))
+    d3 = sum(np.exp(beta*rewards_XiR_3))**len(D)
+    p3 = n3/d3
+
+    Z = p1 + p2 + p3
+    b = [p1/Z, p2/Z, p3/Z]
     return b
 
-def birl_belief(beta, D, Xi_R1, Xi_R2):
+def birl_belief(beta, D, Xi_R1, Xi_R2, Xi_R3):
     rewards_D = np.asarray([Reward(xi,1) for xi in D], dtype = np.float32)    
     rewards_XiR = np.asarray([Reward(xi,1) for xi in Xi_R1], dtype = np.float32)    
     rewards_D_2 = np.asarray([Reward(xi,2) for xi in D], dtype = np.float32)    
     rewards_XiR_2 = np.asarray([Reward(xi,2) for xi in Xi_R2], dtype = np.float32)
+    rewards_D_3 = np.asarray([Reward(xi,3) for xi in D], dtype = np.float32)  
+    rewards_XiR_3 = np.asarray([Reward(xi,3) for xi in Xi_R3], dtype = np.float32)
 
-    norm = np.max(np.asarray([np.max(np.abs(rewards_D)),np.max(np.abs(rewards_XiR)),\
-         np.max(np.abs(rewards_D_2)), np.max(np.abs(rewards_XiR_2))]))
-    
+    # norm = np.max(np.asarray([np.max(np.abs(rewards_D)),np.max(np.abs(rewards_XiR)),\
+    #      np.max(np.abs(rewards_D_2)), np.max(np.abs(rewards_XiR_2)),\
+    #      np.max(np.abs(rewards_D_3)), np.max(np.abs(rewards_XiR_3))]))
+
     rewards_D = rewards_D/np.max(np.abs(rewards_D))
     rewards_XiR = rewards_XiR/np.max(np.abs(rewards_XiR))
     rewards_D_2 = rewards_D_2/np.max(np.abs(rewards_D_2))
     rewards_XiR_2 = rewards_XiR_2/np.max(np.abs(rewards_XiR_2))
+    rewards_D_3 = rewards_D_3/np.max(np.abs(rewards_D_3))
+    rewards_XiR_3 = rewards_XiR_3/np.max(np.abs(rewards_XiR_3))
 
     n1 = np.exp(beta*sum(rewards_D))
     d1 = np.exp(beta*sum(rewards_XiR))
@@ -109,83 +128,104 @@ def birl_belief(beta, D, Xi_R1, Xi_R2):
     d2 = np.exp(beta*sum(rewards_XiR_2))
     p2 = n2/d2
 
-    Z = p1 + p2
-    b = [p1/Z, p2/Z]
+    # Reward for crashing in the middle
+    n3 = np.exp(beta*sum(rewards_D_3))
+    d3 = np.exp(beta*sum(rewards_XiR_3))
+    p3 = n3/d3
+
+    Z = p1 + p2 + p3
+    b = [p1/Z, p2/Z, p3/Z]
     return b
 
 
 def main():
 
-    b_e_t = []
-    b_n_t = []
-    b_o_t = []
-
-
     for t in range(1,11):
-        f1, axs = plt.subplots(nrows=1, ncols=3, figsize=(20, 15))
+        f1, axs = plt.subplots(nrows=3, ncols=3, figsize=(20, 15))
         f1.suptitle('Belief wrt approach using constant delay: ' + str(t))
         width = 0.35
         row = 0
         col = 0
 
-
         #import trajectories (that could be choices)
         D = pickle.load( open( "../data/lander_R1_t_"+ str(t) +".pkl", "rb" ) )
         E = pickle.load( open( "../data/lander_R1_easy_t_"+ str(t) +".pkl", "rb" ) )
-        O_R2 = pickle.load( open( "../data/lander_R2_t_1.pkl", "rb" ) )
         O_R1 = pickle.load( open("../data/lander_R1_t_1.pkl", "rb") )
+        O_R2 = pickle.load( open("../data/lander_R2_t_1.pkl", "rb" ) )
+        O_R3 = pickle.load( open("../data/lander_R3_t_1.pkl", "rb") )
         N = pickle.load( open( "../data/lander_R1_noisy_t_"+ str(t) +".pkl", "rb" ) )
 
         print("Action Delay: ", t)
         #rationality constant. Increasing makes different terms dominate
-        b_e = []
-        b_n = []
-        b_o = []
-        betas = [0.1, 0.5, 1, 5]
+        betas = [0.1, 0.5, 0.75, 1]
         Rewards = ['R1', 'R2']
+
+        b_our_R1 = []
+        b_our_R2 = []
+        b_our_R3 = []
+        b_ut_R1 = []
+        b_ut_R2 = []
+        b_ut_R3 = []
+        b_cl_R1 = []
+        b_cl_R2 = []
+        b_cl_R3 = []
         for beta in betas:
             # Our approach
             Xi_R = D + E
-            b = get_belief(beta, D, Xi_R)
-            b_e.append(b[0])
-            
-
+            b_our = get_belief(beta, D, Xi_R)
+            b_our_R1.append(b_our[0])
+            b_our_R2.append(b_our[1])
+            b_our_R3.append(b_our[2])
 
             # UT approach
             Xi_R = D + N
-            b = get_belief(beta, D, Xi_R)
-            b_n.append(b[0])
-            # axs[col].bar(Rewards, b)
-            # col += 1
+            b_ut = get_belief(beta, D, Xi_R)
+            b_ut_R1.append(b_ut[0])
+            b_ut_R2.append(b_ut[1])
+            b_ut_R3.append(b_ut[2])
 
-            # Classic appraoch
-            b = birl_belief(beta, D, O_R1, O_R2)
-            b_o.append(b[0])
-            # axs[col].bar(Rewards, b)
+            #Classic approach
+            b_cl = birl_belief(beta, D, O_R1, O_R2, O_R3)
+            b_cl_R1.append(b_cl[0])
+            b_cl_R2.append(b_cl[1])
+            b_cl_R3.append(b_cl[2])
+
         titles = ['Our Approach', 'UT Approach', 'Classic Approach']
-        for i, b in enumerate([b_e, b_n, b_o]):
-            x_axis = np.arange(len(b))
-            axs[col].bar(x_axis, b)
-            axs[col].set_ylim([0, 1])
-            axs[col].set_xticks(x_axis)
-            axs[col].set_xticklabels(betas)
-            axs[col].set_title(titles[i])
-            axs[col].set_xlabel('Beta')
-            axs[col].set_ylabel('Belief')
-            col += 1   
-        plt.savefig('../plots/modified_reward/beliefs_t_' + str(t) + '.png')
+        for i, b in enumerate([b_our_R1, b_ut_R1, b_cl_R1]):
+            x_axis = np.arange(len(betas))
+            axs[0,col].bar(x_axis, b)
+            axs[0,col].set_ylim([0, 1])
+            axs[0,col].set_xticks(x_axis)
+            axs[0,col].set_xticklabels(betas)
+            axs[0,col].set_title(titles[i])
+            axs[0,col].set_xlabel('Beta')
+            axs[0,col].set_ylabel('Belief')
+            col += 1
+        col = 0   
+        for i, b in enumerate([b_our_R2, b_ut_R2, b_cl_R2]):
+            x_axis = np.arange(len(betas))
+            axs[1,col].bar(x_axis, b)
+            axs[1,col].set_ylim([0, 1])
+            axs[1,col].set_xticks(x_axis)
+            axs[1,col].set_xticklabels(betas)
+            axs[1,col].set_title(titles[i])
+            axs[1,col].set_xlabel('Beta')
+            axs[1,col].set_ylabel('Belief')
+            col += 1
+        col = 0
+        for i, b in enumerate([b_our_R3, b_ut_R3, b_cl_R3]):
+            x_axis = np.arange(len(betas))
+            axs[2,col].bar(x_axis, b)
+            axs[2,col].set_ylim([0, 1])
+            axs[2,col].set_xticks(x_axis)
+            axs[2,col].set_xticklabels(betas)
+            axs[2,col].set_title(titles[i])
+            axs[2,col].set_xlabel('Beta')
+            axs[2,col].set_ylabel('Belief')
+            col += 1 
+        f1.tight_layout()     
+        f1.savefig('../plots/modified_reward/beliefs_t_' + str(t) + '.png')
         # plt.show()
-
-        b_e_t.append(b_e)
-        b_n_t.append(b_n)
-        b_o_t.append(b_o)
-        
-        
-        
-
-
-    
-
 
 if __name__ == "__main__":
     main()
