b79e275cb5acfdf1687456ab0cd56b2c75336a54
Ananth <ajonnavittula@wpi.edu>
2020-09-23 00:22:33 -0400

    Dev: tinkering with teleop for cars

--- swarm/main.py
+++ swarm/main.py
@@ -1,6 +1,7 @@
 from env_1 import SimpleEnv
 import numpy as np
 import time
+import pybullet as p
 
 env = SimpleEnv()
 start_time = time.time()
@@ -9,6 +10,15 @@ while curr_time < 2400*np.pi:
     curr_time = time.time() - start_time
     x = 0.0
     y = 0.0
+    events = p.getKeyboardEvents()
+    key_codes = events.keys()
+    for key in key_codes:
+        if key == "w":
+            x = 0.5
+        if key == "a":
+            y = 0.5
+        if key == "d":
+            y = 0
     action1 = [x, y]
     # action2 = [-0.5, 0]
     # action3 = [0.5, 0.5]
